242ad2ad02fbd6a866a4989dbf065437cfc26fff
orderly-guoqing <guoqing@orderly.network>
2026-07-07 08:23:51 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_ID": "demo",
-  "VITE_ORDERLY_BROKER_NAME": "MimeTest",
+  "VITE_ORDERLY_BROKER_NAME": "FormDataDEX",
   "VITE_BROKER_EOA_ADDRESS": "0x5f60f2b78063d882203234efc7cac0ed26b686b8",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
@@ -14,7 +14,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_DISABLE_EVM_WALLETS": "false",
   "VITE_DISABLE_SOLANA_WALLETS": "false",
   "VITE_WALLETCONNECT_PROJECT_ID": "",
-  "VITE_APP_NAME": "MimeTest",
+  "VITE_APP_NAME": "FormDataDEX",
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "false",
   "VITE_HAS_SECONDARY_LOGO": "false",
